7ba3aab592d1534e028c224aaefbf25909a65cf1
Artur-Niezgoda <110778550+Artur-Niezgoda@users.noreply.github.com>
2024-12-23 05:04:47 +0100

    Install PostgreSQL database adaptor.

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+"""
+Calculator functions
+"""
+
+def add(x, y):
+    """ Add x and y and return result"""
+
+    return x + y
+
+def subtract(x,y):
+    """ Subtracts y and x and return y-x result"""
+
+    return y-x
